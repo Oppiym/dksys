17104d4362ea3d3866f1ecc51fd825be3131c3ec
Oppiym <oppiym@gmail.com>
2019-10-16 18:32:09 +0300

    Home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,29 @@
 <template>
   <v-container>
-    <v-layout row wrap>
-        <v-flex xs12 sm6>
+    <v-layout row wrap class="mb-2">
+        <v-flex xs12 class="mb-2">
             <v-btn large class="primary"
             to="/events">Посмотреть мероприятия</v-btn>
         </v-flex>
-        <v-flex xs12 sm6>
+        <v-flex xs12 class="mb-2">
             <v-btn large class="primary"
             to="/events/new">Создать мероприятие</v-btn>
         </v-flex>
     </v-layout>
     <v-layout row wrap>
-        <v-carousel>
+        <v-flex xs12>
+         <v-carousel>
             <v-carousel-item
-                 src="src"
-            ></v-carousel-item>
-        <v-carousel>
+                v-for="event in events"
+                :src="event.imageURL"
+                :key="event.id">
+                <div class="title" >
+                    {{ event.title }}
+                </div>
+                </v-carousel-item>
+
+         </v-carousel>
+        </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -23,6 +31,16 @@
 <script>
 export default {
   data: () => ({
+      events:[{ imageURL: 'http://s7606.pcdn.co/wp-content/uploads/2016/04/New-Yorker-1_tcm87-19419.jpg', id: 'fdfsfd',title:'meetup in new york' },
+  { imageURL: 'https://i3.photo.2gis.com/images/branch/1/140737493066473_b179.jpg', id: 'fd56d',title:'meetup ' }
+]
   })
 };
 </script>
+<style scoped>
+.title {
+    position: absolute;
+    bottom: 50px;
+    background-color: red;
+}
+</style>
