e191adfb26213129080080e1136a34328972a08b
Oppiym <oppiym@gmail.com>
2019-10-21 22:37:25 +0300

    ds

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,45 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app clipped>
-      <v-list dense>
-        <v-list-item
-          v-for="item in menuItems"
-          :key="item.title"
-          :to="item.link"
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+<v-app id="inspire">
+  <v-navigation-drawer v-model="drawer" app clipped>
+    <v-list dense>
+      <v-list-item v-for="item in menuItems" :key="item.title" :to="item.link">
+        <v-list-item-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 
-    <v-app-bar app clipped-left color="indigo" dark>
-      <router-link to="/"
-        ><v-img
-          :src="require('@/assets/logo.png')"
-          contain
-          height="48"
-          width="48"
-          max-width="48"
-          @click="$vuetify.goTo(0)"
-      /></router-link>
+  <v-app-bar app clipped-left color="indigo" dark>
+    <router-link to="/">
+      <v-img :src="require('@/assets/logo.png')" contain height="48" width="48" max-width="48" @click="$vuetify.goTo(0)" />
+    </router-link>
 
-      <v-toolbar-title>
-        <router-link to="/" tag="span" style="cursor: pointer">
-          Культурный Центр Москворечье
-        </router-link>
-      </v-toolbar-title>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn
-          text
-           v-for="item in menuItems"
-          :key="item.title"
-          :to="item.link"
-        >
-          <v-icon left>{{ item.icon }}</v-icon>
-          {{ item.title }}
-        </v-btn>
-      </v-toolbar-items>
-    </v-app-bar>
+    <v-toolbar-title>
+      <router-link to="/" tag="span" style="cursor: pointer">
+        Культурный Центр Москворечье
+      </router-link>
+    </v-toolbar-title>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn text v-for="item in menuItems" :key="item.title" :to="item.link">
+        <v-icon left>{{ item.icon }}</v-icon>
+        {{ item.title }}
+      </v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
 
-    <v-content> <router-view></router-view> </v-content>
-    <v-footer color="indigo" app>
-      <span class="white--text">&copy; 2019 Oppiym</span>
-    </v-footer>
-  </v-app>
+  <v-content>
+    <router-view></router-view>
+  </v-content>
+  <v-footer color="indigo" app>
+    <span class="white--text">&copy; 2019 Oppiym</span>
+  </v-footer>
+</v-app>
 </template>
 
 <script>
@@ -62,12 +49,31 @@ export default {
   },
   data: () => ({
     drawer: false,
-    menuItems: [
-      { icon: "supervisor_account", title: "Мероприятия", link: "/events" },
-      { icon: "room", title: "Добавить мероприятие", link: "/events/new" },
-      { icon: "person", title: "Профиль", link: "/profile" },
-      { icon: "face", title: "Зарегистрироваться", link: "/signup" },
-      { icon: "lock_open", title: "Войти", link: "/signin" }
+    menuItems: [{
+        icon: "supervisor_account",
+        title: "Мероприятия",
+        link: "/events"
+      },
+      {
+        icon: "room",
+        title: "Добавить мероприятие",
+        link: "/events/new"
+      },
+      {
+        icon: "person",
+        title: "Профиль",
+        link: "/profile"
+      },
+      {
+        icon: "face",
+        title: "Зарегистрироваться",
+        link: "/signup"
+      },
+      {
+        icon: "lock_open",
+        title: "Войти",
+        link: "/signin"
+      }
     ]
   })
 };
--- src/store.js
+++ src/store.js
@@ -4,61 +4,63 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {
-        loadedEvents: [{
-                imageURL: 'http://s7606.pcdn.co/wp-content/uploads/2016/04/New-Yorker-1_tcm87-19419.jpg',
-                id: 'fdfsfd',
-                title: 'meetup in new york',
-                date: '2018-12-30'
-            },
-            {
-                imageURL: 'https://i3.photo.2gis.com/images/branch/1/140737493066473_b179.jpg',
-                id: 'fd56d',
-                title: 'meetup',
-                date: '2019-11-20'
-            }
-        ],
-        user: [ {
-            id: 'sddadsadsa',
-            registeredEvents: ['asfaffedwgewerfe']
-        }]
-    },
-    mutations: {
-        createEvent (state,payload) {
-            state.loadedEvents.push(payload)
-        }
-    },
-    actions: {
-        createEvent ({commit}, payload) {
-            const event = {
-                title: payload.title,
-                location: payload.location,
-                imageURL: payload.imageURL,
-                description: payload.description,
-                date: payload.date,
-                id:'34324twetewefewfewfewfefweewfefwtfwefgt'
-            }
-
-            commit('createEvent', event)
-        }
-
-    },
-    getters: {
-        loadedEvents(state) {
-            return state.loadedEvents.sort((eventA, eventB) => {
-                return eventA.date > eventB.date
-            })
-        },
-        featuredEvents(state,getters) {
-            return getters.loadedEvents.slice(0,5);
-        },
-        loadedEvent(state) {
-            return (eventId) => {
-                return state.loadedEvents.find((event) => {
-                    return event.id === eventId
-                })
-            }
-        }
+  state: {
+    loadedEvents: [{
+        imageURL: 'http://s7606.pcdn.co/wp-content/uploads/2016/04/New-Yorker-1_tcm87-19419.jpg',
+        id: 'fdfsfd',
+        title: 'meetup in new york',
+        date: '2018-12-30'
+      },
+      {
+        imageURL: 'https://i3.photo.2gis.com/images/branch/1/140737493066473_b179.jpg',
+        id: 'fd56d',
+        title: 'meetup',
+        date: '2019-11-20'
+      }
+    ],
+    user: [{
+      id: 'sddadsadsa',
+      registeredEvents: ['asfaffedwgewerfe']
+    }]
+  },
+  mutations: {
+    createEvent(state, payload) {
+      state.loadedEvents.push(payload)
     }
+  },
+  actions: {
+    createEvent({
+      commit
+    }, payload) {
+      const event = {
+        title: payload.title,
+        location: payload.location,
+        imageURL: payload.imageURL,
+        description: payload.description,
+        date: payload.date,
+        id: '34324twetewefewfewfewfefweewfefwtfwefgt'
+      }
+
+      commit('createEvent', event)
+    }
+
+  },
+  getters: {
+    loadedEvents(state) {
+      return state.loadedEvents.sort((eventA, eventB) => {
+        return eventA.date > eventB.date
+      })
+    },
+    featuredEvents(state, getters) {
+      return getters.loadedEvents.slice(0, 5);
+    },
+    loadedEvent(state) {
+      return (eventId) => {
+        return state.loadedEvents.find((event) => {
+          return event.id === eventId
+        })
+      }
+    }
+  }
 
 })
